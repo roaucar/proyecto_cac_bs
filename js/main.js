ab485d5b3f6b6d764173ea47caa17896ed297125
roAAucar <rdelcarmen1982@gmail.com>
2023-10-27 15:33:04 -0300

    guardo mi tercera version

--- js/main.js
+++ js/main.js
@@ -1,60 +1,42 @@
-//Formulario Conviertete en Orador
-/*
-const form = document.querySelector('form');
-const inputNombre = document.getElementById('nomOrador');
-const inputApellido = document.getElementById('apeOrador');
-const textareaComentario = document.getElementById('comentOrador');
-
-const formulario = document.getElementById('formlario');
-formulario.addEventListener('submit',function(e){
-  e.preventDefault();
-  let nombre = inputNombre.value;
-  let apellido = inputApellido.value;
-  let comentario = textareaComentario.value;
-  console.log('Nombre: ' + nombre);
-  console.log('Apellido: ' + apellido);
-  console.log('Comentario: ' + comentario);
-
-  inputNombre.value = '';
-  inputApellido.value = '';
-  textareaComentario.value = '';
-});*/
-
-
-// Formulario de compra
 const formCompra = document.getElementById('formCompra');
-formCompra.addEventListener('submit',function(e){
+const inputCantidad = document.getElementById('inputCantidad');
+const inputCategoria = document.getElementById('inputCategoria');
+
+formCompra.addEventListener('submit', function (e) {
   e.preventDefault();
   let nombre = document.getElementById('inputnombre').value;
   let apellido = document.getElementById('inputapellido').value;
   let email = document.getElementById('inputemail').value;
-  console.log(nombre,apellido,email);
-})
+  console.log('Nombre:', nombre);
+  console.log('Apellido:', apellido);
+  console.log('Email:', email);
+});
+
 function calcularTotal() {
   const cantidad = parseInt(inputCantidad.value);
   const categoria = parseInt(inputCategoria.value);
   const valorTicket = 200;
   let descuento = 0;
-   
+
   switch (categoria) {
     case 1:
-      descuento = 0.8; 
+      descuento = 0.8;
       break;
     case 2:
-      descuento = 0.5; 
+      descuento = 0.5;
       break;
     case 3:
-      descuento = 0.15; 
+      descuento = 0.15;
       break;
     default:
-      descuento = 0; 
+      descuento = 0;
       break;
   }
-const resltadoTotal = document.getElementById('resultadoTotal')
-  const total = cantidad * valorTicket * (1 - descuento);
-  resltadoTotal.value = '$' + total.toFixed(2);
-  console.log('Total a pagar: $' + total.toFixed(2));
 
+  const resultadoTotal = document.getElementById('resultadoTotal');
+  const total = cantidad * valorTicket * (1 - descuento);
+  resultadoTotal.value = '$' + total.toFixed(2);
+  console.log('Total a pagar: $' + total.toFixed(2));
 }
 
 inputCantidad.addEventListener('change', calcularTotal);
